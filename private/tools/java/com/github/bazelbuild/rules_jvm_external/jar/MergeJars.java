f59df4760260818d613381ee518593a15a99980a
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2024-07-05 12:41:40 -0400

    fix issue with copying binaries by creating new entries

--- private/tools/java/com/github/bazelbuild/rules_jvm_external/jar/MergeJars.java
+++ private/tools/java/com/github/bazelbuild/rules_jvm_external/jar/MergeJars.java
@@ -243,7 +243,7 @@ public class MergeJars {
             continue;
           }
 
-          jos.putNextEntry(entry);
+          jos.putNextEntry(new ZipEntry(entry.getName()));
           try (InputStream is = aar.getInputStream(entry)) {
             ByteStreams.copy(is, jos);
           }
